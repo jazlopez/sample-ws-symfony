4b86f158b2b082c15d27ee08197b523a530b633f
Jaziel Lopez <juan.jaziel@gmail.com>
2016-04-09 21:28:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+app
+===
+
+A Symfony project created on April 10, 2016, 12:57 am.
---
+++ src/AppBundle/Topic/SampleTopic.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Jaziel Lopez <juan.jaziel@gmail.com>
+ * Software Developer
+ * Tijuana Area, Mexico, 4/9/16 7:44 PM.$year
+ *
+ * https://jlopez.mx
+ * https://linked.in/in/jlopez.mx
+ *
+ */
+
+namespace AppBundle\Topic;
+
+use Gos\Bundle\WebSocketBundle\Topic\TopicInterface;
+use Ratchet\ConnectionInterface;
+use Ratchet\Wamp\Topic;
+use Gos\Bundle\WebSocketBundle\Router\WampRequest;
+
+class SampleTopic implements TopicInterface
+{
+    /**
+     * This will receive any Subscription requests for this topic.
+     *
+     * @param ConnectionInterface $connection
+     * @param Topic $topic
+     * @param WampRequest $request
+     * @return void
+     */
+    public function onSubscribe(ConnectionInterface $connection, Topic $topic, WampRequest $request)
+    {
+
+        // $topic->broadcast(['msg' => $connection->resourceId . " has joined " . $topic->getId()]);
+    }
+
+    /**
+     * This will receive any UnSubscription requests for this topic.
+     *
+     * @param ConnectionInterface $connection
+     * @param Topic $topic
+     * @param WampRequest $request
+     * @return void
+     */
+    public function onUnSubscribe(ConnectionInterface $connection, Topic $topic, WampRequest $request)
+    {
+        //this will broadcast the message to ALL subscribers of this topic.
+        // $topic->broadcast(['msg' => $connection->resourceId . " has left " . $topic->getId()]);
+    }
+
+
+    /**
+     * This will receive any Publish requests for this topic.
+     *
+     * @param ConnectionInterface $connection
+     * @param Topic $topic
+     * @param WampRequest $request
+     * @param $event
+     * @param array $exclude
+     * @param array $eligible
+     * @return mixed|void
+     */
+    public function onPublish(ConnectionInterface $connection, Topic $topic, WampRequest $request, $event, array $exclude, array $eligible)
+    {
+
+        // $topic->broadcast($event, [$connection->WAMP->sessionId]);
+        $topic->broadcast($event);
+    }
+
+    /**
+     * Like RPC is will use to prefix the channel
+     * @return string
+     */
+    public function getName()
+    {
+        return 'sample.channel';
+    }
+}
